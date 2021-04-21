cd8a4aa4aceb9ecdecd049477ca2e3c8cd80d067
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-04-21 02:54:24 -0600

    [ADD] The config file for Firebase Client

---
+++ lib/firebase/client.ts
@@ -0,0 +1,20 @@
+import firebaseClient from 'firebase';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const FIREBASE_CONFIG = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+};
+
+if (typeof window !== 'undefined' && !firebaseClient.apps.length) {
+  firebaseClient.initializeApp(FIREBASE_CONFIG);
+}
+
+export const GOOGLE_AUTH_PROVIDER = new firebaseClient.auth.GoogleAuthProvider();
+
+export default firebaseClient;
