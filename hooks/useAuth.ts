1c5825d1fb2362d4eaeb9155174a786dba56abb5
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-04-21 02:52:53 -0600

    [ADD] The useAuth hook

---
+++ hooks/useAuth.ts
@@ -0,0 +1,13 @@
+import { useContext } from 'react';
+
+import AuthContext from 'contexts/AuthContext';
+
+export const useAuth = () => {
+  const authContext = useContext(AuthContext);
+
+  if (!authContext) {
+    throw new Error('useAuth must be used within a AuthProvider');
+  }
+
+  return authContext;
+};
